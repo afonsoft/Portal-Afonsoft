8724da092f93d373a69c9607a284af50ddb21d1e
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-02-21 10:01:56 -0300

    Correção da URL

--- aspnet-core/src/Afonsoft.Portal.Web.Mvc/Startup/PortalWebMvcModule.cs
+++ aspnet-core/src/Afonsoft.Portal.Web.Mvc/Startup/PortalWebMvcModule.cs
@@ -27,7 +27,7 @@ namespace Afonsoft.Portal.Web.Startup
 
         public override void PreInitialize()
         {
-            Configuration.Modules.AbpWebCommon().MultiTenancy.DomainFormat = _appConfiguration["App:WebSiteRootAddress"] ?? "http://localhost:9902/";
+            Configuration.Modules.AbpWebCommon().MultiTenancy.DomainFormat = _appConfiguration["App:WebSiteRootAddress"] ?? "https://portal.afonsoft.com.br/";
             Configuration.Modules.AspNetZero().LicenseCode = _appConfiguration["AbpZeroLicenseCode"];
             Configuration.Navigation.Providers.Add<AppNavigationProvider>();
         }
--- aspnet-core/test/Afonsoft.Portal.ConsoleApiClient/Program.cs
+++ aspnet-core/test/Afonsoft.Portal.ConsoleApiClient/Program.cs
@@ -19,7 +19,7 @@ namespace Afonsoft.Portal.ConsoleApiClient
 
     class Program
     {
-        private const string ServerUrlBase = "http://localhost:9903/";
+        private const string ServerUrlBase = "https://portal.afonsoft.com.br/";
         
         // If you have changed "Configuration.MultiTenancy.TenantIdResolveKey" in your web app, use the same value here. 
         private const string TenantIdResolveKey = "Abp.TenantId";
